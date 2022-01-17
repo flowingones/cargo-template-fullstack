f089a86e5f86caf91a699c75f2177d98db53ee1f
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-01-17 20:43:14 +0000

    Update dependencies version

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export * from "https://deno.land/x/cargo@0.1.12/mod.ts";
-export * from "https://deno.land/x/cargo@0.1.12/http/mod.ts";
+export * from "https://deno.land/x/cargo@0.1.13/mod.ts";
+export * from "https://deno.land/x/cargo@0.1.13/http/mod.ts";
 
-export { factory } from "https://raw.githubusercontent.com/flowingones/cargo-parcel/main/jsx/factory.ts";
-export { ParcelPage } from "https://raw.githubusercontent.com/flowingones/cargo-parcel/main/http/route.ts";
+export { factory } from "https://deno.land/x/cargo_parcel@0.1.13/jsx/factory.ts";
+export { Page } from "https://deno.land/x/cargo_parcel@0.1.13/http/route.ts";
 
 export { Root } from "./Root.tsx";
