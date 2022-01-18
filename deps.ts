26de96b58d700534523d5dc4e91a71874751109e
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-01-18 17:23:17 +0000

    Bump depenencies version

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export * from "https://deno.land/x/cargo@0.1.13/mod.ts";
-export * from "https://deno.land/x/cargo@0.1.13/http/mod.ts";
+export * from "https://deno.land/x/cargo@0.1.14/mod.ts";
+export * from "https://deno.land/x/cargo@0.1.14/http/mod.ts";
 
-export { factory } from "https://deno.land/x/cargo_parcel@0.1.13/jsx/factory.ts";
-export { Page } from "https://deno.land/x/cargo_parcel@0.1.13/http/route.ts";
+export { factory } from "https://deno.land/x/cargo_parcel@0.1.14/jsx/factory.ts";
+export { Page } from "https://deno.land/x/cargo_parcel@0.1.14/http/route.ts";
 
 export { Root } from "./Root.tsx";
