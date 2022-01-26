fe4b5e51b5ab20913df2334ebb4da9d655c31088
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-01-26 15:42:16 +0000

    Upgrade Cargo version

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export * from "https://deno.land/x/cargo@0.1.18/mod.ts";
-export * from "https://deno.land/x/cargo@0.1.18/http/mod.ts";
+export * from "https://deno.land/x/cargo@0.1.19/mod.ts";
+export * from "https://deno.land/x/cargo@0.1.19/http/mod.ts";
 
-export { factory } from "https://deno.land/x/cargo_parcel@0.1.18/jsx/factory.ts";
-export { Page } from "https://deno.land/x/cargo_parcel@0.1.18/http/route.ts";
+export { factory } from "https://deno.land/x/cargo_parcel@0.1.19/jsx/factory.ts";
+export { Page } from "https://deno.land/x/cargo_parcel@0.1.19/http/route.ts";
 
 export { Root } from "./Root.tsx";
